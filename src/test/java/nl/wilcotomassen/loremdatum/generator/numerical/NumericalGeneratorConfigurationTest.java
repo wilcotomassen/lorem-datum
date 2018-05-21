7c608ab0f10f60a0c1ae84c00ce04e5bedd5d404
Wilco Tomassen <wilco@wilcotomassen.nl>
2018-05-21 17:56:26 +0200

    Finalized NumericationConfiguration unit tests

--- src/test/java/nl/wilcotomassen/loremdatum/generator/numerical/NumericalGeneratorConfigurationTest.java
+++ src/test/java/nl/wilcotomassen/loremdatum/generator/numerical/NumericalGeneratorConfigurationTest.java
@@ -7,26 +7,41 @@ public abstract class NumericalGeneratorConfigurationTest {
 	
 	/**
 	 * Valid lower bound test values for variation (all these values should be accepted)
+	 * Correct range: [-1.0, 0.0]
 	 */
-	public final static double[] VARIATION_LOWER_BOUND_VALID_TEST_VALUES = { -Double.MIN_NORMAL, -.0001, -0,001, -0.01, -0.1, 
-			-1.0};
+	public final static double[] VARIATION_LOWER_BOUND_VALID_TEST_VALUES = { 
+		0.0, // Greatest allowed value
+		-Double.MIN_NORMAL,	-.0001, -0.001, -0.01, -0.1, // Range of allowable values 
+		-1.0 // Smallest allowed value
+	}; 
 	
 	/**
 	 * Invalid lower bound test values for variation (all these values should be rejected)
 	 */
-	public final static double[] VARIATION_LOWER_BOUND_INVALID_TEST_VALUES = { Double.NaN, Double.NEGATIVE_INFINITY, 
-			Double.POSITIVE_INFINITY };
+	public final static double[] VARIATION_LOWER_BOUND_INVALID_TEST_VALUES = { 
+		Double.NaN, Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY, // Invalid numbers 
+		-(1.0 + Double.MIN_VALUE), Double.MIN_VALUE, // Smallest out of bound values
+		.0001, 0,001, 0.01, 0.1, 1.0 // Positive values
+	}; 
 	
 	/**
 	 * Valid upper bound test values for variation (all these values should be accepted)
+	 * Correct range: [0.0, 1.0]
 	 */
-	public final static double[] VARIATION_UPPER_BOUND_VALID_TEST_VALUES = { Double.MIN_NORMAL, .0001, 0,001, 0.01, 0.1, 1.0 };
+	public final static double[] VARIATION_UPPER_BOUND_VALID_TEST_VALUES = { 
+		0.0, // Smallest allowed value
+		Double.MIN_NORMAL, .0001, 0,001, 0.01, 0.1, // Range of allowed values 
+		1.0 // Greatest allowed value
+	}; 
 	
 	/**
 	 * Invalid upper bound test values for variation (all these values should be rejected)
 	 */
-	public final static double[] VARIATION_UPPER_BOUND_INVALID_TEST_VALUES = { Double.NaN, Double.NEGATIVE_INFINITY, 
-			Double.POSITIVE_INFINITY };
+	public final static double[] VARIATION_UPPER_BOUND_INVALID_TEST_VALUES = { 
+		Double.NaN, Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY, // NaN and infinity
+		1.0 + Double.MIN_VALUE, -Double.MIN_VALUE, // Smallest out of bound values
+		-.0001, -0.001, -0.01, -0.1, -1.0 // Negative values 
+	};
 	
 	/**
 	 * Fetch configuration of a subclass from {@link NumericalGeneratorConfiguration}
@@ -71,32 +86,32 @@ public abstract class NumericalGeneratorConfigurationTest {
 		}
 	}
 
-	@Test
+	@Test(expectedExceptions = IllegalArgumentException.class)
 	public final void testVariationLowerBoundsInvalidValues() {
 		for (int i = 0; i < VARIATION_LOWER_BOUND_INVALID_TEST_VALUES.length; i++) {
 			final double testValue = VARIATION_LOWER_BOUND_INVALID_TEST_VALUES[i];
-			testVariationLowerBoundEquals(getConfiguration(null, null, testValue, null), testValue);
+			getConfiguration(null, null, testValue, null);
 		}
 	}
 	
 	@Test
 	public final void testVariationUpperBoundDefault() {
-		testVariationUpperBoundEquals(getConfiguration(),NumericalGeneratorConfiguration.VARIATION_UPPER_BOUND_DEFAULT );
+		testVariationUpperBoundEquals(getConfiguration(), NumericalGeneratorConfiguration.VARIATION_UPPER_BOUND_DEFAULT);
 	}
 	
 	@Test
 	public final void testVariationUpperBoundValidValues() {
 		for (int i = 0; i < VARIATION_UPPER_BOUND_VALID_TEST_VALUES.length; i++) {
 			final double testValue = VARIATION_UPPER_BOUND_VALID_TEST_VALUES[i];
-			testVariationUpperBoundEquals(getConfiguration(null, null, testValue, null), testValue);
+			testVariationUpperBoundEquals(getConfiguration(null, null, null, testValue), testValue);
 		}
 	}
 	
-	@Test
+	@Test(expectedExceptions = IllegalArgumentException.class)
 	public final void testVariationUpperBoundInvalidValues() {
 		for (int i = 0; i < VARIATION_UPPER_BOUND_INVALID_TEST_VALUES.length; i++) {
 			final double testValue = VARIATION_UPPER_BOUND_INVALID_TEST_VALUES[i];
-			testVariationUpperBoundEquals(getConfiguration(null, null, testValue, null), testValue);
+			getConfiguration(null, null, null, testValue);
 		}
 	}
 	
