cfeec8b0eddfa96d75179555d341fed5c553a9f0
Wilco Tomassen <wilco@wilcotomassen.nl>
2018-05-21 17:18:26 +0200

    Refactor WIP on numerical tests

--- src/test/java/nl/wilcotomassen/loremdatum/generator/numerical/NumericalGeneratorConfigurationTest.java
+++ src/test/java/nl/wilcotomassen/loremdatum/generator/numerical/NumericalGeneratorConfigurationTest.java
@@ -6,6 +6,29 @@ import org.testng.annotations.Test;
 public abstract class NumericalGeneratorConfigurationTest {
 	
 	/**
+	 * Valid lower bound test values for variation (all these values should be accepted)
+	 */
+	public final static double[] VARIATION_LOWER_BOUND_VALID_TEST_VALUES = { -Double.MIN_NORMAL, -.0001, -0,001, -0.01, -0.1, 
+			-1.0};
+	
+	/**
+	 * Invalid lower bound test values for variation (all these values should be rejected)
+	 */
+	public final static double[] VARIATION_LOWER_BOUND_INVALID_TEST_VALUES = { Double.NaN, Double.NEGATIVE_INFINITY, 
+			Double.POSITIVE_INFINITY };
+	
+	/**
+	 * Valid upper bound test values for variation (all these values should be accepted)
+	 */
+	public final static double[] VARIATION_UPPER_BOUND_VALID_TEST_VALUES = { Double.MIN_NORMAL, .0001, 0,001, 0.01, 0.1, 1.0 };
+	
+	/**
+	 * Invalid upper bound test values for variation (all these values should be rejected)
+	 */
+	public final static double[] VARIATION_UPPER_BOUND_INVALID_TEST_VALUES = { Double.NaN, Double.NEGATIVE_INFINITY, 
+			Double.POSITIVE_INFINITY };
+	
+	/**
 	 * Fetch configuration of a subclass from {@link NumericalGeneratorConfiguration}
 	 * 
 	 * @param seed
@@ -41,9 +64,19 @@ public abstract class NumericalGeneratorConfigurationTest {
 	}
 	
 	@Test
-	public final void testVariationLowerBoundPositiveSetterNotNull() {
-		final double variationLowerBound = .001;
-		testVariationLowerBoundEquals(getConfiguration(null, null, variationLowerBound, null), variationLowerBound);
+	public final void testVariationLowerBoundsValidValues() {
+		for (int i = 0; i < VARIATION_LOWER_BOUND_VALID_TEST_VALUES.length; i++) {
+			final double testValue = VARIATION_LOWER_BOUND_VALID_TEST_VALUES[i];
+			testVariationLowerBoundEquals(getConfiguration(null, null, testValue, null), testValue);
+		}
+	}
+
+	@Test
+	public final void testVariationLowerBoundsInvalidValues() {
+		for (int i = 0; i < VARIATION_LOWER_BOUND_INVALID_TEST_VALUES.length; i++) {
+			final double testValue = VARIATION_LOWER_BOUND_INVALID_TEST_VALUES[i];
+			testVariationLowerBoundEquals(getConfiguration(null, null, testValue, null), testValue);
+		}
 	}
 	
 	@Test
@@ -52,16 +85,19 @@ public abstract class NumericalGeneratorConfigurationTest {
 	}
 	
 	@Test
-	public final void testVariationUpperBoundPositiveSetterNotNull() {
-		final double variationUpperBound = .3;
-		testVariationUpperBoundEquals(getConfiguration(null, null, null, variationUpperBound), variationUpperBound);
+	public final void testVariationUpperBoundValidValues() {
+		for (int i = 0; i < VARIATION_UPPER_BOUND_VALID_TEST_VALUES.length; i++) {
+			final double testValue = VARIATION_UPPER_BOUND_VALID_TEST_VALUES[i];
+			testVariationUpperBoundEquals(getConfiguration(null, null, testValue, null), testValue);
+		}
 	}
 	
 	@Test
-	public final void testVariationUpperBoundNegativeSetterNotNull() {
-		//@TODO : Should not work, as it exceeds the bounds
-		final double variationUpperBound = -.3;
-		testVariationUpperBoundEquals(getConfiguration(null, null, null, variationUpperBound), variationUpperBound);
+	public final void testVariationUpperBoundInvalidValues() {
+		for (int i = 0; i < VARIATION_UPPER_BOUND_INVALID_TEST_VALUES.length; i++) {
+			final double testValue = VARIATION_UPPER_BOUND_INVALID_TEST_VALUES[i];
+			testVariationUpperBoundEquals(getConfiguration(null, null, testValue, null), testValue);
+		}
 	}
 	
 }
